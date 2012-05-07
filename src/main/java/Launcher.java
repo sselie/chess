7307433ebadc4bc2c67e305781fbea8719662526
ericminio <eric.mignot@gmail.com>
2012-05-06 20:35:12 -0400

    all queens and pawns on the board
    --HG--
    branch : gui

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,10 +1,36 @@
 import gui.GameFrame;
 
+import static builders.PawnBuilder.aPawn;
+import static builders.QueenBuilder.aQueen;
+
 public class Launcher {
 
-    
-    public static void main(String...arg) {
+
+    public static void main(String... arg) {
         GameFrame game = new GameFrame();
         game.setVisible( true );
+
+        game.display(
+                aPawn().white().on( "a2" ).build(),
+                aPawn().white().on( "b2" ).build(),
+                aPawn().white().on( "c2" ).build(),
+                aPawn().white().on( "d2" ).build(),
+                aPawn().white().on( "e2" ).build(),
+                aPawn().white().on( "F2" ).build(),
+                aPawn().white().on( "g2" ).build(),
+                aPawn().white().on( "h2" ).build(),
+
+                aPawn().black().on( "a7" ).build(),
+                aPawn().black().on( "b7" ).build(),
+                aPawn().black().on( "c7" ).build(),
+                aPawn().black().on( "d7" ).build(),
+                aPawn().black().on( "e7" ).build(),
+                aPawn().black().on( "F7" ).build(),
+                aPawn().black().on( "g7" ).build(),
+                aPawn().black().on( "h7" ).build(),
+
+                aQueen().white().on( "d1" ).build(),
+                aQueen().black().on( "d8" ).build()
+        );
     }
 }
