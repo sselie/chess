705c27a6a54c7514f630e919688c1183ea260e2c
Francois Perron <fperron@gmail.com>
2014-04-27 19:50:19 -0400

    Initial positions rule is unit tested in order to remove 'domain rule tested from UI' smell

--- src/test/java/chess/rules/InitialPositionsTest.java
+++ src/test/java/chess/rules/InitialPositionsTest.java
@@ -1,42 +1,72 @@
 package chess.rules;
 
-import gui.GameFrame;
+import chess.*;
 import imhotep.Imhotep;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import support.GameDriver;
+import support.PieceTester;
+
+import java.util.Collections;
 
 import static chess.rules.InitialPositions.initialPositions;
-import static support.GameDriver.on;
-import static support.GameDriver.rook;
 
-@Imhotep(level="UI")
+@Imhotep(level="Unit")
 public class InitialPositionsTest {
 
-    GameFrame game;
-    GameDriver board;
+    private Pieces pieces;
 
     @Before
     public void
-    showGame() {
-        game = new GameFrame();
-        game.setVisible( true );
-        board = new GameDriver();
-        game.display( initialPositions() );
-    }
-    @After
-    public void
-    hideFrame() {
-        game.setVisible( false );
+    TheInitialPositions() {
+        pieces = new Pieces();
+        Collections.addAll(pieces, initialPositions());
     }
 
     @Test public void
-    displaysRooks() {
-        board.hasWhite( rook(), on( "a1" ) );
-        board.hasWhite( rook(), on( "h1" ) );
-        board.hasBlack( rook(), on( "a8" ) );
-        board.hasBlack( rook(), on( "h8" ) );
+    WhitePiecesAreAtTheRightPositionsOnLines1And2() {
+        PieceAt("a1").IsWhite().IsA(Roock.class);
+        PieceAt("b1").IsWhite().IsA(Knight.class);
+        PieceAt("c1").IsWhite().IsA(Bishop.class);
+        PieceAt("d1").IsWhite().IsA(Queen.class);
+        PieceAt("e1").IsWhite().IsA(King.class);
+        PieceAt("f1").IsWhite().IsA(Bishop.class);
+        PieceAt("g1").IsWhite().IsA(Knight.class);
+        PieceAt("h1").IsWhite().IsA(Roock.class);
+
+        PieceAt("a2").IsWhite().IsA(Pawn.class);
+        PieceAt("b2").IsWhite().IsA(Pawn.class);
+        PieceAt("c2").IsWhite().IsA(Pawn.class);
+        PieceAt("d2").IsWhite().IsA(Pawn.class);
+        PieceAt("e2").IsWhite().IsA(Pawn.class);
+        PieceAt("f2").IsWhite().IsA(Pawn.class);
+        PieceAt("g2").IsWhite().IsA(Pawn.class);
+        PieceAt("h2").IsWhite().IsA(Pawn.class);
     }
 
+    @Test public void
+    BlackPiecesAreAtTheRightPositionsOnLines7And8() {
+        PieceAt("a8").IsBlack().IsA(Roock.class);
+        PieceAt("b8").IsBlack().IsA(Knight.class);
+        PieceAt("c8").IsBlack().IsA(Bishop.class);
+        PieceAt("d8").IsBlack().IsA(Queen.class);
+        PieceAt("e8").IsBlack().IsA(King.class);
+        PieceAt("f8").IsBlack().IsA(Bishop.class);
+        PieceAt("g8").IsBlack().IsA(Knight.class);
+        PieceAt("h8").IsBlack().IsA(Roock.class);
+
+        PieceAt("a7").IsBlack().IsA(Pawn.class);
+        PieceAt("b7").IsBlack().IsA(Pawn.class);
+        PieceAt("c7").IsBlack().IsA(Pawn.class);
+        PieceAt("d7").IsBlack().IsA(Pawn.class);
+        PieceAt("e7").IsBlack().IsA(Pawn.class);
+        PieceAt("f7").IsBlack().IsA(Pawn.class);
+        PieceAt("g7").IsBlack().IsA(Pawn.class);
+        PieceAt("h7").IsBlack().IsA(Pawn.class);
+    }
+
+    private PieceTester PieceAt(String position)
+    {
+        Piece piece = pieces.getPieceWithPosition(position);
+        return new PieceTester(piece);
+    }
 }
